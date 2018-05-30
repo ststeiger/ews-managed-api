950d763a755b8fcc1a7aa74fa301f7df5d4d71c4
ststeiger <quandary82@hailmail.net>
2018-05-30 17:34:29 +0200

    Added Exchange 2013 Samples

---
+++ ExchangeCodeSamples/CopyExampleCode.cs
@@ -0,0 +1,55 @@
+﻿
+namespace ExchangeCodeSamples
+{
+
+
+    public class ExampleCode
+    {
+
+
+        public static void Copy()
+        {
+            string targetDir = @"D:\username\Documents\Visual Studio 2017\Projects\ews-managed-api\ExchangeCodeSamples\Examples";
+
+            string dir = @"D:\username\Downloads\Exchange 2013 101 Code Samples";
+
+            string[] baseDirz = System.IO.Directory.GetDirectories(dir, "Exchange*", System.IO.SearchOption.TopDirectoryOnly);
+
+            foreach (string baseDir in baseDirz)
+            {
+                string[] projDirz = System.IO.Directory.GetDirectories(baseDir, "Ex*", System.IO.SearchOption.AllDirectories);
+                if (projDirz.Length > 1)
+                    System.Console.WriteLine(projDirz);
+
+                foreach (string projDir in projDirz)
+                {
+                    string[] filez = System.IO.Directory.GetFiles(projDir, "Ex15_*.cs");
+                    if (filez.Length > 1)
+                        System.Console.WriteLine(filez);
+
+                    foreach (string file in filez)
+                    {
+                        System.Console.WriteLine(file);
+                        string fileName = System.IO.Path.GetFileName(file);
+                        if (fileName.StartsWith("Ex15_", System.StringComparison.OrdinalIgnoreCase))
+                            fileName = fileName.Substring("Ex15_".Length);
+                        else
+                            System.Console.WriteLine("argh");
+
+                        string target = System.IO.Path.Combine(targetDir, fileName);
+
+                        System.IO.File.Copy(file, target, true);
+                    } // Next file 
+
+                } // Next projDir 
+
+            } // Next baseDir 
+
+            System.Console.WriteLine("finished");
+        } // End Sub Copy 
+
+
+    } // End Class ExampleCode 
+
+
+} // End Namespace ExchangeCodeSamples
---
+++ ExchangeCodeSamples/WinFormsWorkaround.cs
@@ -0,0 +1,36 @@
+﻿
+namespace System.Windows.Forms
+{
+
+
+    public enum DialogResult: int 
+    {
+        OK
+    } 
+
+    public class OpenFileDialog
+    {
+
+        public DialogResult ShowDialog()
+        {
+            return DialogResult.OK;
+        }
+
+        public System.IO.Stream OpenFile()
+        {
+            return null;
+        }
+
+
+        public string Filter { get; set; } = "All files (*.*)|*.*";
+        public string InitialDirectory { get; set; } = "C:";
+        public string Title { get; set; } = "Select an attachment";
+        public int FilterIndex { get; set; } = 1;
+
+        public string SafeFileName { get; set; }
+        public string FileName { get; set; }
+
+    }
+
+
+}
