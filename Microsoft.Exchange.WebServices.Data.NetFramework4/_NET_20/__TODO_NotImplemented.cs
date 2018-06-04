a39d39112e39a5cefa141abb2f7db0c84616db37
Me <no.spam@example.com>
2018-06-04 23:56:18 +0200

    Implement time subst

--- Microsoft.Exchange.WebServices.Data.NetFramework4/_NET_20/__TODO_NotImplemented.cs
+++ Microsoft.Exchange.WebServices.Data.NetFramework4/_NET_20/__TODO_NotImplemented.cs
@@ -6,12 +6,88 @@ namespace System
 {
 
 
+    internal class NativeMethods
+    {
+
+        /// <summary>
+        /// The SystemTime structure represents a date and time using individual members 
+        /// for the month, day, year, weekday, hour, minute, second, and millisecond.
+        /// </summary>
+        [Runtime.InteropServices.StructLayoutAttribute(Runtime.InteropServices.LayoutKind.Sequential)]
+        public struct SystemTime
+        {
+            public short year;
+            public short month;
+            public short dayOfWeek;
+            public short day;
+            public short hour;
+            public short minute;
+            public short second;
+            public short milliseconds;
+        }
+
+        /// <summary>
+        /// The TimeZoneInformation structure specifies information specific to the time zone.
+        /// </summary>
+        [Runtime.InteropServices.StructLayout(Runtime.InteropServices.LayoutKind.Sequential, CharSet = Runtime.InteropServices.CharSet.Unicode)]
+        public struct TimeZoneInformation
+        {
+            /// <summary>
+            /// Current bias for local time translation on this computer, in minutes. The bias is the difference, in minutes, between Coordinated Universal Time (UTC) and local time. All translations between UTC and local time are based on the following formula: 
+            /// <para>UTC = local time + bias</para>
+            /// <para>This member is required.</para>
+            /// </summary>
+            public int bias;
+            /// <summary>
+            /// Pointer to a null-terminated string associated with standard time. For example, "EST" could indicate Eastern Standard Time. The string will be returned unchanged by the GetTimeZoneInformation function. This string can be empty.
+            /// </summary>
+            [Runtime.InteropServices.MarshalAs(Runtime.InteropServices.UnmanagedType.ByValTStr, SizeConst = 32)]
+            public string standardName;
+            /// <summary>
+            /// A SystemTime structure that contains a date and local time when the transition from daylight saving time to standard time occurs on this operating system. If the time zone does not support daylight saving time or if the caller needs to disable daylight saving time, the wMonth member in the SystemTime structure must be zero. If this date is specified, the DaylightDate value in the TimeZoneInformation structure must also be specified. Otherwise, the system assumes the time zone data is invalid and no changes will be applied.
+            /// <para>To select the correct day in the month, set the wYear member to zero, the wHour and wMinute members to the transition time, the wDayOfWeek member to the appropriate weekday, and the wDay member to indicate the occurence of the day of the week within the month (first through fifth).</para>
+            /// <para>Using this notation, specify the 2:00a.m. on the first Sunday in April as follows: wHour = 2, wMonth = 4, wDayOfWeek = 0, wDay = 1. Specify 2:00a.m. on the last Thursday in October as follows: wHour = 2, wMonth = 10, wDayOfWeek = 4, wDay = 5.</para>
+            /// </summary>
+            public SystemTime standardDate;
+            /// <summary>
+            /// Bias value to be used during local time translations that occur during standard time. This member is ignored if a value for the StandardDate member is not supplied. 
+            /// <para>This value is added to the value of the Bias member to form the bias used during standard time. In most time zones, the value of this member is zero.</para>
+            /// </summary>
+            public int standardBias;
+            /// <summary>
+            /// Pointer to a null-terminated string associated with daylight saving time. For example, "PDT" could indicate Pacific Daylight Time. The string will be returned unchanged by the GetTimeZoneInformation function. This string can be empty.
+            /// </summary>
+            [Runtime.InteropServices.MarshalAs(Runtime.InteropServices.UnmanagedType.ByValTStr, SizeConst = 32)]
+            public string daylightName;
+            /// <summary>
+            /// A SystemTime structure that contains a date and local time when the transition from standard time to daylight saving time occurs on this operating system. If the time zone does not support daylight saving time or if the caller needs to disable daylight saving time, the wMonth member in the SystemTime structure must be zero. If this date is specified, the StandardDate value in the TimeZoneInformation structure must also be specified. Otherwise, the system assumes the time zone data is invalid and no changes will be applied.
+            /// <para>To select the correct day in the month, set the wYear member to zero, the wHour and wMinute members to the transition time, the wDayOfWeek member to the appropriate weekday, and the wDay member to indicate the occurence of the day of the week within the month (first through fifth).</para>
+            /// </summary>
+            public SystemTime daylightDate;
+            /// <summary>
+            /// Bias value to be used during local time translations that occur during daylight saving time. This member is ignored if a value for the DaylightDate member is not supplied. 
+            /// <para>This value is added to the value of the Bias member to form the bias used during daylight saving time. In most time zones, the value of this member is –60.</para>
+            /// </summary>
+            public int daylightBias;
+        }
+
+
+
+        [Runtime.InteropServices.DllImport("kernel32.dll")]
+        public static extern uint GetTimeZoneInformation(out TimeZoneInformation lpTimeZoneInformation);
+
+    }
+
+
     public sealed partial class TimeZoneInfo
     {
         private static void PopulateAllSystemTimeZones(CachedData cachedData)
         {
             // https://docs.microsoft.com/en-us/previous-versions/sql/sql-server-2005/ms171251(v=sql.90)
             // https://docs.microsoft.com/en-us/dotnet/standard/datetime/enumerate-time-zones
+
+            
+
             throw new NotImplementedException();
         }
 
@@ -33,12 +109,26 @@ namespace System
 
         private static TimeZoneInfo GetLocalTimeZone(CachedData cachedData)
         {
+            if (System.Environment.OSVersion.Platform == PlatformID.Unix)
+            {
+                // /etc/localtime
+                // match with /usr/share/zoneinfo/ 
+            }
+            else
+            {
+                NativeMethods.TimeZoneInformation tzi;
+                NativeMethods.GetTimeZoneInformation(out tzi);
+            }
+            
             throw new NotImplementedException();
             return null;
         }
 
         public static TimeZoneInfo FindSystemTimeZoneById(string id)
         {
+            // https://stackoverflow.com/questions/3621540/convert-reg-binary-data-to-reg-tzi-format
+            // HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Time Zones
+            // /usr/share/zoneinfo/ 
             throw new NotImplementedException();
             return null;
         }
