f77d5695353a64ef555d888a3a3f6a71a69d9571
Me <no.spam@example.com>
2018-06-13 20:38:17 +0200

    ObjectNotFound Fix

--- Microsoft.Exchange.WebServices.Data.NetFramework4/_NET_20/ActiveDirectoryObjectNotFoundException.cs
+++ Microsoft.Exchange.WebServices.Data.NetFramework4/_NET_20/ActiveDirectoryObjectNotFoundException.cs
@@ -5,7 +5,9 @@ namespace System.Directoryservices.ActiveDirectory
     public class ActiveDirectoryObjectNotFoundException
         :System.Exception 
     {
-        
+        public ActiveDirectoryObjectNotFoundException()
+            :base()
+        { }
     }
     
 }
